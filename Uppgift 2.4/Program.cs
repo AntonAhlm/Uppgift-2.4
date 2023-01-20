f9415cc5c62f8a65e749732cc38d0fc5d8e453d9
Anton <>
2023-01-20 09:30:27 +0100

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 2 tal. ");
+            Console.Write("Tal 1: ");
+            int Tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Tal 2: ");
+            int Tal2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Tal1 + " + " + Tal2 + " = " + (Tal1+Tal2));
+            Console.WriteLine(Tal1 + " * " + Tal2 + " = " + (Tal1*Tal2));
+
+        }
+    }
+}
